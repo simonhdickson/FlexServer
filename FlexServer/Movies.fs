9464103e64e639a26d46df425212f3cf9e45f423
Simon Dickson <simon@simonhdickson.com>
2017-03-11 15:53:51 +0000

    Initial commit

---
+++ FlexServer/Movies.fs
@@ -0,0 +1,55 @@
+﻿module Movies
+
+open System.IO
+open Suave
+open Suave.Filters
+open Suave.Json
+open Suave.Operators
+open Suave.Successful
+open Suave.Utils
+open Suave.Writers
+open Newtonsoft.Json
+open Suave.Sockets.Control
+open SuaveExtensions
+
+let folder = """D:\Content\Video\Movies"""
+
+let write status dto : WebPart =
+    status (JsonConvert.SerializeObject dto) >=> setMimeType "application/json; charset=utf-8"
+
+type Movie = {
+    name:string
+    url:string
+}
+
+let movies =
+    Directory.EnumerateFiles folder
+    |> Seq.map (fun file ->
+        { name = Path.GetFileNameWithoutExtension file
+          url = sprintf "http://127.0.0.1:8080/movies/%s" <| Path.GetFileName file })
+
+let page = sprintf """<head>
+  <link href="http://vjs.zencdn.net/5.17.0/video-js.css" rel="stylesheet">
+
+  <!-- If you'd like to support IE8 -->
+  <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
+</head>
+
+<body>
+  <video id="my-video" class="video-js" controls preload="auto" poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
+    <source src="%s" type='video/mp4'>
+    <p class="vjs-no-js">
+      To view this video please enable JavaScript, and consider upgrading to a web browser that
+      <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+    </p>
+  </video>
+
+  <script src="http://vjs.zencdn.net/5.17.0/video.js"></script>
+</body>"""
+
+let webPart : WebPart<HttpContext> =
+    choose [
+        GET >=> path "/movies" >=> write OK movies
+        GET >=> pathScan "/movies/%s" (fun f -> partialFile <| Path.Combine(folder, f))
+        GET >=> pathScan "/movies/%s/player" (fun f -> OK (page (sprintf "http://127.0.0.1:8080/movies/%s" f))) >=> setMimeType "text/html; charset=utf-8" 
+    ]
---
+++ FlexServer/SuaveExtensions.fs
@@ -0,0 +1,87 @@
+﻿module SuaveExtensions
+
+open System
+open System.Text
+open System.IO
+open Suave
+open Suave.Sockets.Control
+open Suave.Sockets
+open Suave.Embedded
+
+type RangedStream(stream:Stream, start, limit, ?disposeInner) =
+    inherit Stream()
+    do stream.Position <- start
+    let endPosition = min stream.Length (stream.Position + limit)
+    let maxRead count =
+        let maxCount = endPosition - stream.Position
+        if count > maxCount then maxCount else count
+    override __.CanRead = stream.CanRead
+    override __.CanSeek = stream.CanSeek
+    override __.CanWrite = stream.CanWrite
+    override __.Flush() = stream.Flush()
+    override __.Length with get () = int64 (endPosition - start)
+    override __.Position with get () = stream.Position and set value = stream.Position <- value
+    override __.Seek(offset, origin) = stream.Seek(offset, origin)
+    override __.SetLength(value) = failwith ""
+    override __.Read(buffer, offset, count) = stream.Read(buffer, offset, int <| maxRead (int64 count))
+    override __.Write(buffer, offset, count) = failwith ""
+    override __.CanTimeout = stream.CanTimeout
+    override __.ReadTimeout with get () = stream.ReadTimeout and set value = stream.ReadTimeout <- value
+    override __.WriteTimeout with get () = stream.WriteTimeout and set value = stream.WriteTimeout <- value
+    override __.BeginRead(buffer, offset, count, callback, state) =
+        stream.BeginRead(buffer, offset, int <| maxRead (int64 count), callback, state)
+    override __.Close() =
+        disposeInner
+        |> Option.iter (fun i -> if i then stream.Close())
+
+let sendFile (start:int, finish) fileName (compression : bool) (ctx : HttpContext) =
+    let writeFile file (conn, _) = socket {
+        let length = finish - start
+        let getFs = fun path ->
+            use fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+            fs.Seek(int64 start, SeekOrigin.Begin) |> ignore
+            let buffer = Array.zeroCreate length
+            fs.Read(buffer, 0, length) |> ignore
+            fs.Flush()
+            new MemoryStream(buffer) :> Stream
+        let getLm = fun path -> FileInfo(path).LastWriteTime
+        let! (encoding, fs) = Compression.transformStream file getFs getLm compression ctx.runtime.compressionFolder ctx
+        try
+            match encoding with
+            | Some n ->
+                let! (_,conn) = asyncWriteLn (String.Concat [| "Content-Encoding: "; n.ToString() |]) conn
+                let! (_,conn) = asyncWriteLn (sprintf "Content-Length: %d\r\n" (fs : Stream).Length) conn
+                let! conn = flush conn
+                if  ctx.request.``method`` <> HttpMethod.HEAD && fs.Length > 0L then
+                    do! transferStream conn fs
+                return conn
+            | None ->
+                let! (_,conn) = asyncWriteLn (sprintf "Content-Length: %d\r\n" (fs : Stream).Length) conn
+                let! conn = flush conn
+                if  ctx.request.``method`` <> HttpMethod.HEAD && fs.Length > 0L then
+                    do! transferStream conn fs
+                return conn
+      finally
+        fs.Dispose()
+    }
+    { ctx with
+        response =
+          { ctx.response with
+              status = HTTP_200.status
+              content = SocketTask (writeFile fileName) } }
+    |> succeed
+
+let (|ContentRange|_|)  (context:HttpContext) =
+    match context.request.header "content-range" with
+    | Choice1Of2 range ->
+        let result = range.Split(' ', '/', '-')
+        if result.Length = 4 then
+            Some (int result.[1], int result.[2])
+        else
+            None
+    | Choice2Of2 _ -> None
+
+let partialFile file (context:HttpContext) =
+    match context with
+    | ContentRange range -> sendFile range file true context
+    | _ -> Files.file file context
